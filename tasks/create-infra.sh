1bfe1b8f326b66d5591006431db2dfcf651b24fd
Ubuntu <ubuntu@jumpbox.openstack.local>
2018-03-26 07:45:58 +0000

    Adding terraform

--- tasks/create-infra.sh
+++ tasks/create-infra.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
-
+ROOT=$PWD
 set -eu
-echo "hrello phani"
+
+function main(){
+
+  terraform init "$ROOT/phani-pipelines/terraform"
+
+  terraform plan \
+   -var "os_tenant_name=${OS_PROJECT_NAME}" \
+   -var "os_username=${OS_USERNAME}" \
+   -var "os_password=${OS_PASSWORD}" \
+   -var "os_auth_url=${OS_AUTH_URL}" \
+   -var "os_region=${OS_REGION_NAME}" \
+   -var "os_domain_name=${OS_USER_DOMAIN_NAME}" \
+   -out "terraform.tfplan" \
+   -state "terraform-state/terraform.tfstate" \
+   "$ROOT/phani-pipelines/terraform"
+
+
+  terraform apply \
+    -state-out "$ROOT/create-infrastructure-output/terraform.tfstate" \
+    -parallelism=5 \
+    terraform.tfplan
+
+}
+main
---
+++ terraform/network.tf
@@ -0,0 +1,16 @@
+resource "openstack_networking_network_v2" "phani_net" {
+  name = "${var.prefix}-infra-net"
+  region = "${var.os_region}"
+  admin_state_up = "true"
+}
+
+resource "openstack_networking_subnet_v2" "phani_subnet" {
+  network_id = "${openstack_networking_network_v2.phani_net.id}"
+  region = "${var.os_region}"
+  cidr = "${var.phani_subnet_cidr}"
+  ip_version = 4
+  enable_dhcp = true
+  dns_nameservers = [
+    "${var.infra_dns}"
+  ]
+}
